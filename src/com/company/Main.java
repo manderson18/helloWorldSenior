7e1a78e6852daf166fcf1c81b590da591f2e5c3c
Maxwell Anderson <spamformaxwell@gmail.com>
2017-08-30 10:19:50 -0400

    Information Gathering
    Totally not a way to get away with stealing someones identity.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,13 +2,31 @@ package com.company;
 import java.util.*;
 public class Main {
 
-    public static void main(String[] args) {
-	System.out.println("Hello World");
-
-        Scanner in = new Scanner(System.in);
-
-
-
+    public static void main(String[] args){
+	 Scanner in = new Scanner(System.in);
+	    
+	    System.out.println("What is your name? ");
+		String name = in.newLine();
+	    System.out.println("How old are you? ");
+		int age = in.newInt();
+	    System.out.println("Please enter your favorite number: ");
+	    	double fav = in.newDouble();
+	    System.out.println("What is your favorite color? ");
+	    	String color = in.newInt();
+	    System.out.println("What is your mother's maiden name? ")
+		String maidenname = in.newLine();
+	    System.out.println("What was the name of your first pet? ");
+	    	String pet = in.newLine();
+	    System.out.println("What was the name of your Highschool? ");
+	    	String HS = in.newLine();
+	    System.out.println("When were you born? ")
+		String BDAY = in.newLine();
+	    
+	System.out.println("Hello"+name+"!"+"I am Maxwell Anderson, I have written this neat little app that allows me to commit identity fruad, I mean errrr...allows me to gather some information about my peers");
+	    
+	    
+	    
+	
 
 
 
